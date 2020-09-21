b705bddbc2feb6996fb3ef03c0dd4ef1b0749a47
pandaachu <pandaachu@gmail.com>
2020-09-21 17:03:05 +0800

    abuot update

--- src/views/user/About.vue
+++ src/views/user/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
-    <div class="container">
-      <img src="../../../public/images/apothecary-87-6uTS4cpkqjM-unsplash.jpg" alt="">
-      <p>
+    <div class="container my-5">
+      <img class="mb-5" src="../../../public/images/apothecary-87-6uTS4cpkqjM-unsplash.jpg" alt="">
+      <p class="w-50 m-auto">
         FOREST 的香氛系列是以其中一個植物作主要基底，加上其他植物成份所調配出來的香氛。
         有蠟燭、香水以及精油來搭配使用，讓生活變得不一樣。
       </p>
