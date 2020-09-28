1154e8ae014a2cd5fbc39d73a6ecde6a35391d11
pandaachu <pandaachu@gmail.com>
2020-09-28 17:21:37 +0800

    update products filter

--- src/views/user/Products.vue
+++ src/views/user/Products.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="l-products text-light">
+  <div class="l-products text-light container mt-5">
     <Loading :active.sync="isLoading"></Loading>
-    <nav class="navbar navbar-expand-lg navbar-dark justify-content-center border border-left-0 border-right-0 border-top border-bottom">
-      <div class="navbar-nav flex-row overflow-auto navbar-custom-scroll">
-        <a class="nav-item nav-link text-nowrap px-2" href="#">研究中 |</a>
-        <a class="nav-item nav-link text-nowrap px-2" href="#">研究中 |</a>
-        <a class="nav-item nav-link text-nowrap px-2 active" href="#">研究中  |<span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link text-nowrap px-2" href="#">研究中 |</a>
-        <a class="nav-item nav-link text-nowrap px-2" href="#">研究中</a>
+    <div class="row justify-content-center">
+      <div class="input-group col-5">
+        <select class="form-control w-25 btn btn-secondary text-white" v-model="input.category">
+          <option>全部</option>
+          <option>香精油</option>
+          <option>蠟燭</option>
+        </select>
+        <input v-model.trim="input.title" type="text" placeholder="搜尋" class="form-control btn btn-outline-secondary w-75">
       </div>
-    </nav>
+    </div>
+    <!-- <nav class="navbar navbar-expand-lg navbar-dark justify-content-center border border-left-0 border-right-0 border-top border-bottom"> -->
+      <!-- <div class="navbar-nav flex-row overflow-auto navbar-custom-scroll mr-3">
+        <a class="nav-item nav-link text-nowrap px-2" href="#">全部 |</a>
+        <a class="nav-item nav-link text-nowrap px-2" href="#">香精油 |</a>
+        <a class="nav-item nav-link text-nowrap px-2 active" href="#">研究中  |<span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link text-nowrap px-2" href="#">蠟燭</a>
+      </div>
+      <div class="input-group-prepend">
+        <input type="text" class="form-control btn btn-outline-secondary">
+      </div> -->
+      <!-- <div class="input-group mb-3">
+        <select class="custom-select col-sm-1">
+          <option selected>選擇...</option>
+          <option value="1">全部</option>
+          <option value="2">香精油</option>
+          <option value="3">蠟燭</option>
+        </select>
+        <div class="input-group-prepend">
+          <input type="text" class="form-control btn btn-outline-secondary">
+        </div>
+      </div> -->
+      <!-- <div class="input-group">
+        <select v-model="input.type" class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <option value="">全部</option>
+          <option value="">香精油</option>
+          <option value="">蠟燭</option>
+        </select>
+        <input type="text" v-model.trim="input.title">
+      </div> -->
+    <!-- </nav> -->
     <div class="container mt-md-5 mt-3 mb-7">
       <div class="row">
-        <div class="col-md-4" v-for="product in products" :key="product.id">
+        <div class="col-md-4" v-for="product in titleFilter" :key="product.id">
           <div class="card bg-transparent border-gray mb-4 position-relative rounded-0">
             <img :src="product.imageUrl[0]" class="card-img-top rounded-0" alt="...">
             <!-- 之後再加我的最愛 -->
@@ -49,7 +80,11 @@ export default {
         imageUrl: [] // 資料結構
       },
       products: [],
-      pagination: {}
+      pagination: {},
+      input: {
+        category: '全部',
+        title: ''
+      }
     }
   },
   methods: {
@@ -70,7 +105,7 @@ export default {
       this.$http.get(api)
         .then(res => {
           this.isLoading = false
-          // console.log(res);
+          // console.log(res)
           // 對應遠端傳回來的資料
           this.products = res.data.data
           this.pagination = res.data.meta.pagination
@@ -92,6 +127,32 @@ export default {
         this.products = res.data.data
       })
     this.getProducts()
+  },
+  computed: {
+    categoryFilter () {
+      if (this.input.category !== '全部') { // 如果 input.category 不是全部
+        return this.products.filter(item => {
+          return item.category === this.input.category
+        })
+      } else {
+        return this.products
+      }
+    },
+    titleFilter () {
+      if (this.input.title) { // input.title 有資料的話
+        return this.categoryFilter.filter(item => {
+          // 先轉小寫 toLowerCase()
+          const content = item.title.toLowerCase()
+          const keyword = this.input.title.toLowerCase()
+          // 在陣列中找到的第一個元素索引值；沒找到則為 -1。
+          // 不等於 -1 -> 有找到資料，再進行比對
+          // 比對 keyword 跟 content的字串
+          return content.indexOf(keyword) !== -1
+        })
+      } else {
+        return this.categoryFilter
+      }
+    }
   }
 }
 
