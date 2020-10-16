bacd06de8c809e3a12f0ba521731de8e46a1924d
pandaachu <pandaachu@gmail.com>
2020-10-16 14:37:10 +0800

    update homepage

--- src/views/user/Home.vue
+++ src/views/user/Home.vue
@@ -18,14 +18,15 @@
           生活，從美好開始
         </h2>
         <h2 class="l-home__title color-text">
-          Life is Getting <span class="splitText"></span><br> with Forest
+          Life is <br class="d-block d-md-none"> Getting <br class="d-block d-md-none">  <span class="splitText"></span><br> with Forest
         </h2>
       </div>
     </section>
-    <div class="l-home__sectionbg pb-5">
-      <section class="l-home__collection container pt-md-5">
+    <!-- collection -->
+    <section class="l-home__collection pt-md-5">
+      <div class="container">
         <div class="row">
-          <div class="col-12 titleGroup text-center">
+          <div class="col-12 titleGroup text-center pt-5">
             <h2
               class="titleGroup__title letter-spacing-l"
               data-aos="custom-fade-up"
@@ -105,152 +106,104 @@
             </router-link>
           </div>
         </div>
-      </section>
-      <hr style="margin-bottom:14rem">
-      <section class="l-home__function pt-5">
-        <div class="container ">
-          <div class="titleGroup text-center">
-            <h2
-              class="titleGroup__title letter-spacing-l"
-              data-aos="custom-fade-up"
-              data-aos-duration="3000"
-              data-aos-delay="400"
-            >
-              不同的場合
-            </h2>
-            <h6
-              class="titleGroup__suptitle letter-spacing-xxl font-weight-light"
-              style="margin-left:20px"
-              data-aos="custom-fade-up"
-              data-aos-duration="3000"
-              data-aos-delay="500"
-            >
-              搭配不同的香氣
-            </h6>
-          </div>
-          <div class="d-flex flex-column">
-            <div>
-              <div href="#" class="imageFrameGroup hoverGroup position-relative d-flex justify-content-center text-secondary mb-4 mb-md-0">
-                  <image-frame></image-frame>
-                  <h4
-                    class="imageFrameGroup__title hoverGroup__underline position-absolute optimaNovaFont letter-spacing-xl hover-underline pb-2"
-                    data-aos="custom-fade-up"
-                    data-aos-duration="3000"
-                    data-aos-delay="200"
-                  >
-                    Sleep
-                    <span class="h6">/ 睡眠</span>
-                  </h4>
-                  <p
-                    class="hoverGroup__showText position-absolute m-0 text-center"
-                    style="top:40%;transform: translateY(50%);z-index: 1;"
-                  >
-                    難以進入睡眠狀態的時候， <br>
-                    搭配寧神靜心的香味，<br>
-                    讓香氣飄散到屋裡的每個角落，讓你安然入眠
-                  </p>
-                  <img class="imageFrameGroup__image hoverGroup__image position-absolute" src="../../../public/images/function/function-1.png" alt="">
-              </div>
-            </div>
-            <div class="d-flex flex-column flex-md-row justify-content-md-center mt-md-n5">
-              <div href="#" class="imageFrameGroup hoverGroup position-relative d-flex justify-content-center mr-0 mr-md-5 text-secondary mb-4 mb-md-0">
-                <image-frame></image-frame>
-                <h4
-                  class="imageFrameGroup__title hoverGroup__underline position-absolute optimaNovaFont letter-spacing-xl hover-underline pb-2"
-                  data-aos="custom-fade-up"
-                  data-aos-duration="3000"
-                  data-aos-delay="300"
-                >
-                  Work
-                  <span class="h6">/ 工作</span>
-                </h4>
-                <p
-                  class="hoverGroup__showText position-absolute m-0 text-center"
-                  style="top:40%;transform: translateY(50%);z-index: 1;"
-                >
-                  需要集中精神於工作時，<br>
-                  搭配提神、增加專注力的香味， <br>
-                  能夠讓你事半功倍，工作也能保持好心情
-                </p>
-                <img class="hoverGroup__image imageFrameGroup__image position-absolute" src="../../../public/images/function/function-2.png" alt="">
-              </div>
-              <div href="#" class="imageFrameGroup hoverGroup position-relative d-flex justify-content-center text-secondary mb-4 mb-md-0">
-                <image-frame></image-frame>
-                <h4
-                  class="imageFrameGroup__title hoverGroup__underline position-absolute optimaNovaFont letter-spacing-xl hover-underline pb-2"
-                  data-aos="custom-fade-up"
-                  data-aos-duration="3000"
-                  data-aos-delay="400"
-                >
-                  Relax
-                  <span class="h6">/ 放鬆</span>
-                </h4>
-                <p
-                  class="hoverGroup__showText position-absolute m-0 text-center"
-                  style="top:40%;transform: translateY(50%);z-index: 1;"
-                >
-                  在休閒的假日裡想放鬆的時候， <br>
-                  搭配柔和舒緩的香味， <br>
-                  隨時隨地放鬆轉換心情，過一個美好的假日！
-                </p>
-                <img class="hoverGroup__image imageFrameGroup__image position-absolute" src="../../../public/images/function/function-3.png" alt="">
-              </div>
-            </div>
-          </div>
+      </div>
+      <hr style="padding-bottom:8rem" class="m-0">
+    </section>
+    <!-- function -->
+    <section class="l-home__function py-5">
+      <div class="container ">
+        <div class="titleGroup text-center">
+          <h2
+            class="titleGroup__title letter-spacing-l"
+            data-aos="custom-fade-up"
+            data-aos-duration="3000"
+            data-aos-delay="400"
+          >
+            不同的場合
+          </h2>
+          <h6
+            class="titleGroup__suptitle letter-spacing-xxl font-weight-light"
+            style="margin-left:20px"
+            data-aos="custom-fade-up"
+            data-aos-duration="3000"
+            data-aos-delay="500"
+          >
+            搭配不同的香氣
+          </h6>
         </div>
-      </section>
-      <hr style="margin-bottom:14rem">
-    </div>
-    <!-- carousel -->
-    <!-- <div class="l-home__carousel py-5">
-      <div class="container border-bottom border-gray">
-        <div id="carouselExampleControls" class="carousel slide mb-5" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div class="row justify-content-center py-7">
-                <div class="col-md-8 d-flex">
-                  <img src="../../../public/images/christin-hume-0MoF-Fe0w0A-unsplash.jpg" alt="" class="rounded-circle mr-5" style="width: 200px; height: 200px; object-fit: cover;">
-                  <div class="d-flex flex-column justify-content-around">
-                    <h4 class="font-weight-bold">精油系列</h4>
-                    <p class="h5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.”</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div class="row justify-content-center py-7">
-                <div class="col-md-8 d-flex">
-                  <img src="../../../public/images/apothecary-87-yu8b_ypXgjw-unsplash.jpg" alt="" class="rounded-circle mr-5" style="width: 200px; height: 200px; object-fit: cover;">
-                  <div class="d-flex flex-column justify-content-around">
-                    <h4 class="font-weight-bold">蠟燭系列</h4>
-                    <p class="h5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.”</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div class="row justify-content-center py-7">
-                <div class="col-md-8 d-flex">
-                  <img src="../../../public/images/kelly-sikkema-XU1-sAMZFqc-unsplash.jpg" alt="" class="rounded-circle mr-5" style="width: 200px; height: 200px; object-fit: cover;">
-                  <div class="d-flex flex-column justify-content-around">
-                    <h4 class="font-weight-bold">香氛系列</h4>
-                    <p class="h5">“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.”</p>
-                  </div>
-                </div>
-              </div>
+        <div class="d-flex flex-column">
+          <div>
+            <div href="#" class="imageFrameGroup hoverGroup position-relative d-flex justify-content-center text-secondary mb-4 mb-md-0">
+                <image-frame></image-frame>
+                <h4
+                  class="imageFrameGroup__title hoverGroup__underline position-absolute optimaNovaFont letter-spacing-xl hover-underline pb-2"
+                  data-aos="custom-fade-up"
+                  data-aos-duration="3000"
+                  data-aos-delay="200"
+                >
+                  Sleep
+                  <span class="h6">/ 睡眠</span>
+                </h4>
+                <p
+                  class="hoverGroup__showText position-absolute m-0 text-center"
+                  style="top:40%;transform: translateY(50%);z-index: 1;"
+                >
+                  難以進入睡眠狀態的時候， <br>
+                  搭配寧神靜心的香味，<br>
+                  讓香氣飄散到屋裡的每個角落，讓你安然入眠
+                </p>
+                <img class="imageFrameGroup__image hoverGroup__image position-absolute" src="../../../public/images/function/function-1.png" alt="">
             </div>
           </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls"  role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
+          <div class="d-flex flex-column flex-md-row justify-content-md-center mt-md-n5">
+            <div href="#" class="imageFrameGroup hoverGroup position-relative d-flex justify-content-center mr-0 mr-md-5 text-secondary mb-4 mb-md-0">
+              <image-frame></image-frame>
+              <h4
+                class="imageFrameGroup__title hoverGroup__underline position-absolute optimaNovaFont letter-spacing-xl hover-underline pb-2"
+                data-aos="custom-fade-up"
+                data-aos-duration="3000"
+                data-aos-delay="300"
+              >
+                Work
+                <span class="h6">/ 工作</span>
+              </h4>
+              <p
+                class="hoverGroup__showText position-absolute m-0 text-center"
+                style="top:40%;transform: translateY(50%);z-index: 1;"
+              >
+                需要集中精神於工作時，<br>
+                搭配提神、增加專注力的香味， <br>
+                能夠讓你事半功倍，工作也能保持好心情
+              </p>
+              <img class="hoverGroup__image imageFrameGroup__image position-absolute" src="../../../public/images/function/function-2.png" alt="">
+            </div>
+            <div href="#" class="imageFrameGroup hoverGroup position-relative d-flex justify-content-center text-secondary mb-4 mb-md-0">
+              <image-frame></image-frame>
+              <h4
+                class="imageFrameGroup__title hoverGroup__underline position-absolute optimaNovaFont letter-spacing-xl hover-underline pb-2"
+                data-aos="custom-fade-up"
+                data-aos-duration="3000"
+                data-aos-delay="400"
+              >
+                Relax
+                <span class="h6">/ 放鬆</span>
+              </h4>
+              <p
+                class="hoverGroup__showText position-absolute m-0 text-center"
+                style="top:40%;transform: translateY(50%);z-index: 1;"
+              >
+                在休閒的假日裡想放鬆的時候， <br>
+                搭配柔和舒緩的香味， <br>
+                隨時隨地放鬆轉換心情，過一個美好的假日！
+              </p>
+              <img class="hoverGroup__image imageFrameGroup__image position-absolute" src="../../../public/images/function/function-3.png" alt="">
+            </div>
+          </div>
         </div>
       </div>
-    </div> -->
+      <hr style="padding-bottom:9rem">
+    </section>
+    <!-- <hr style="margin-bottom:14rem"> -->
   </div>
 </template>
 <style lang="scss" scoped>
